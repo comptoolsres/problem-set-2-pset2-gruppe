de357f5834bb72dfa2bac1ab8af3147ad0ad3735
Ian Culver <iculver@login4.ufhpc>
2020-09-25 09:41:51 -0400

    Added flightdata.sh

---
+++ flightdata.sh
@@ -0,0 +1,21 @@
+#!
+# This will be the shared script and we will each work on a seperate function
+
+unique_airport_codes () {
+	echo "Function unique_airport_codes executed."
+
+}
+
+florida_city_airports () {
+	echo "Function florida_city_airports executed."
+
+}
+
+airport_flight_calculator () {
+	echo "Function airport_flight_calculator executed."
+
+}
+
+gainesville_delayed_flights () {
+	echo "Function gainesville_delayed_flights executed."
+}
