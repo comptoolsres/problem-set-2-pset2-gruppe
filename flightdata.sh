d4d800c29be45a7f6d76f76f7a44ad4bc59f0d3a
Sheng <shengwanhui@login1.ufhpc>
2020-10-01 14:46:38 -0400

    FL Cities function added, Q2 whether delay added

--- flightdata.sh
+++ flightdata.sh
@@ -1,15 +1,51 @@
 #!
 # This will be the shared script and we will each work on a seperate function
+FILE=/blue/bsc4452/share/Class_Files/data/flights.May2018-April2020.csv
+#Question 2: Total flights delayed due to wheather
+
+
+Wheather_Delay (){
+City=$1
+LineCount=$(grep \"GNV\".*\"$City\" $FILE | cut -f22 -d',' | wc -l)
+WheatherDelay=$((LineCount-1))
+echo "There are $WheatherDelay flights to $1 got deplayed due to wheather"
+
+}
+
+Wheather_Delay ATL
+Wheather_Delay CLT
+Wheather_Delay MIA
+
+
 
 unique_airport_codes () {
 	echo "Function unique_airport_codes executed."
 
 }
 
-florida_city_airports () {
-	echo "Function florida_city_airports executed."
+#Question4: #function to find FL cities that have airports
 
+florida_city_airports () {
+#Find FL airports from the origin cities list
+cat $FILE | cut -f4,5 -d,  | grep FL | sort | uniq > Origin
+
+#Find FL airports from the destination cities list
+cat $FILE | cut -f8,9 -d,  | grep FL | sort | uniq > Dest
+
+#list the unique cities in FL
+FLCities=$(paste -d "\n" Origin Dest | sort | uniq)
+
+rm Origin
+rm Dest
 }
+florida_city_airports
+echo "Here is the list of the FL cities that have airport:"
+echo "$FLCities"
+
+echo "Function florida_city_airports executed."
+
+
+
 
 airport_flight_calculator () {
 	echo "Function airport_flight_calculator executed."
