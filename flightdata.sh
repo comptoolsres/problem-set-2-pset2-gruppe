ac6c2e02258ca3e3748f9f304c6f2e9cabf09516
Ian Culver <iculver@login3.ufhpc>
2020-10-02 10:15:58 -0400

    added file variable

--- flightdata.sh
+++ flightdata.sh
@@ -23,6 +23,6 @@ gainesville_delayed_flights () {
 #Additionally grep only the flights with the GNV airport code and this will exclude any lines that do not have GNV as
 #a destination or origin.
 echo "The number of delayed flights to and from gainesville regional airport is:"
-grep GNV flights.May2018-April2020.csv | cut -f13,16 -d, | grep 1.00 | wc -l
+grep GNV $FILE | cut -f13,16 -d, | grep 1.00 | wc -l
 
 }
